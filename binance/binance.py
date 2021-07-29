381e016c22b5e4a90b3dc7cf18b113fcb9e5a137
cjtim <iam@cjtim.com>
2021-07-29 23:46:07 +0700

    enhance: strict return type Binance

--- binance/binance.py
+++ binance/binance.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Tuple
 import requests
 import urllib
 import hmac
@@ -33,7 +34,7 @@ class Binance:
         dict_data = json.loads(resp.content)
         return dict_data
 
-    def get_price_usdt(self, symbols: list):
+    def __get_price_usdt(self) -> Dict[str, str]:
         result = {}
         all_symbols = self.__get_json('/api/v3/ticker/price', {}, False, False)
         for i in all_symbols:
@@ -42,15 +43,15 @@ class Binance:
             })
         return result
 
-    def get_asset(self):
+    def __get_asset(self) -> List[Dict[str, any]]:
         account = self.__get_json('/api/v3/account', {}, True, True)
         arr = [i for i in account['balances'] if float(
             i['free']) != 0 or float(i['locked']) != 0]
         return arr
 
-    def asset_report(self, buy_price):
-        asset = self.get_asset()
-        price = self.get_price_usdt([i['asset'] for i in asset])
+    def asset_report(self, buy_price) -> List[Dict[str, any]]:
+        asset = self.__get_asset()
+        price = self.__get_price_usdt()
         result = []
         for i in asset:
             isCoin = not i['asset'].endswith(
